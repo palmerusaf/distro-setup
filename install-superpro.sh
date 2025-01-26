fdeca7ffe4ae2de1d57c8ec163627ccb69d09838
Branden Palmer <brandenpalmer08@gmail.com>
2025-01-26 20:24:34 +0700

    update superpro install method

--- install-superpro.sh
+++ install-superpro.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 echo "Installing super-productivity"
-curl -LO https://github.com/johannesjo/super-productivity/releases/download/v7.14.3/superProductivity_7.14.3_amd64.deb
-sudo dpkg -i superProductivity_7.14.3_amd64.deb
-rm superProductivity_7.14.3_amd64.deb
+flatpak install com.super_productivity.SuperProductivity
