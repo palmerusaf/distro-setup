162dfbf84890a6260485f356f818402545cc6de3
Branden Palmer <brandenpalmer08@gmail.com>
2025-01-30 16:13:48 +0700

    switch to flatpak for neovim install

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-rm nvim.appimage
+flatpak install io.neovim.nvim
 echo "Installing clipboard support and latex deps..."
 sudo apt-get install -y xclip latexmk texlive
