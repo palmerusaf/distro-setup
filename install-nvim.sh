adf854f9328dc613314288d0f7816cc2cfb2f2f7
Branden Palmer <brandenpalmer08@gmail.com>
2025-02-11 14:57:34 +0700

    add java install

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 flatpak install io.neovim.nvim
+echo "Need to install java so lsps are fully supported"
+sudo apt install default-jre -y
 echo "Installing clipboard support and latex deps..."
 sudo apt-get install -y xclip latexmk texlive
